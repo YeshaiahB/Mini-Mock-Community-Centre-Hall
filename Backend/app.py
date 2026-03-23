71dbcbb82f3fb6a3e4acf0f088dab23265a3a744
Yeshaiah <729960@student.bedford.ac.uk>
2026-03-23 15:23:15 +0000

    Additional Member check added

--- Backend/app.py
+++ Backend/app.py
@@ -9,7 +9,7 @@ def ValidateIfMember():
     IsMember = AskIfMember()
     IsMember = str.strip(str.lower(IsMember))
 
-    if (IsMember == "yes" or IsMember == "no"):
+    if (IsMember == "yes"):
         return True, IsMember
     else:
         return False, IsMember
@@ -23,20 +23,45 @@ def ValidateHoursBooked():
     
     return True, RequestedHours
     
+def AskTypeOfMember():
+    MemberType = str(input("What type of member are you? Standard or Verified "))
+    return MemberType
+
+def ValidateTypeOfMember():
+    MemberType = AskTypeOfMember()
+    MemberType = str.strip(str.lower(MemberType))
+
+    if MemberType == "standard" or MemberType == "verified":
+        return True, MemberType
+    else:
+        return False, MemberType
+    
+
+def ErrorHandler():
+    print("Some of your inputs arent valid! Please try again.")
+    return CalculatePrice()
+
+def PromptMemberShipPurchase():
+    print("Please head to our website to buy a membership! If you can't find it that's too bad cause it dosent exist :) ")
 
 def CalculatePrice():
     MemberValid, IsMember = ValidateIfMember()
     HoursValid, RequestedHours = ValidateHoursBooked()
 
-    print(MemberValid, HoursValid)
-
     if MemberValid == True and HoursValid == True:
         if IsMember == "yes": 
-            print(f'Your total price is: {RequestedHours * MemberPriceRate}')
+            MemberTypeValid, MemberType = ValidateTypeOfMember()
+
+            if not MemberTypeValid:
+                return ErrorHandler()
+
+            if MemberType == "standard":
+                print(f'Your total price is: {RequestedHours * StandardPriceRate}')
+            elif MemberType == "verified":
+                print(f'Your total price is: {RequestedHours * MemberPriceRate}')
         elif IsMember == "no":
-            print(f'Your total price is: {RequestedHours * StandardPriceRate}')
+            PromptMemberShipPurchase()
     else:
-        print("Some of your inputs arent valid! Please try again.")
-        return CalculatePrice()
+        return ErrorHandler()
     
 CalculatePrice()
