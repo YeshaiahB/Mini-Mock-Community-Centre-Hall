759d827a18f99e8fccacdff28219a136d5edfc28
Yeshaiah <729960@student.bedford.ac.uk>
2026-03-23 16:14:57 +0000

    Date validation added. & hours validation fixed.

--- Backend/app.py
+++ Backend/app.py
@@ -1,8 +1,13 @@
 StandardPriceRate = 10
 MemberPriceRate = 7
 
+from datetime import datetime
+
+def ProduceReciept(Price, Name, Age, Date, RequestedDate):
+    print(f'Here is your reciept: \n Total Price: £{Price}, \n Name: {Name}, \n Age: {Age}, \n Your appointment is on: {RequestedDate}, \n Purchase Date: {Date}')
+
 def AskIfMember():
-    IsMember = str(input("Are you a member?"))
+    IsMember = str(input("Are you a member? (Yes / No) "))
     return IsMember
 
 def ValidateIfMember():
@@ -15,16 +20,19 @@ def ValidateIfMember():
         return False, IsMember
 
 def GetHoursBooked():
-    Hours = int(str.strip(input("How many hours do you want to book?")))
+    Hours = str.strip(input("How many hours do you want to book?"))
     return Hours
 
 def ValidateHoursBooked(): 
     RequestedHours = GetHoursBooked()
     
-    return True, RequestedHours
+    if RequestedHours.isnumeric() == True:
+        return True, int(RequestedHours)
+    else:
+        return False, RequestedHours
     
 def AskTypeOfMember():
-    MemberType = str(input("What type of member are you? Standard or Verified "))
+    MemberType = str(input("What type of member are you? (Standard / Verified) "))
     return MemberType
 
 def ValidateTypeOfMember():
@@ -55,13 +63,23 @@ def CalculatePrice():
             if not MemberTypeValid:
                 return ErrorHandler()
 
+            UserName = str.strip(str(input("What is your name?")))
+            Age = int(str.strip(input("How old are you?")))
+            Date = datetime.now()
+
+            print("Please fill in the day you would like to book.")
+            TargetDate = datetime(FormatDateIndex(input("Year: ")), FormatDateIndex(input("Month: ")), FormatDateIndex(input("Day: ")))
+
             if MemberType == "standard":
-                print(f'Your total price is: {RequestedHours * StandardPriceRate}')
+                ProduceReciept(RequestedHours * StandardPriceRate, UserName, Age, Date, TargetDate.strftime("%x"))
             elif MemberType == "verified":
-                print(f'Your total price is: {RequestedHours * MemberPriceRate}')
+                ProduceReciept(RequestedHours * MemberPriceRate, UserName, Age, Date, TargetDate.strftime("%x"))
         elif IsMember == "no":
             PromptMemberShipPurchase()
     else:
         return ErrorHandler()
     
+def FormatDateIndex(Number):
+    return int(str.strip(Number))
+
 CalculatePrice()
